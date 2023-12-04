fa98ff818322a75e1d09b03e80d3d7812508533c
Pieter De Clercq <thepieterdc@users.noreply.github.com>
2023-12-04 18:25:12 +0100

    Cleanup day 4 (#6)

--- Day4/Advanced.hs
+++ Day4/Advanced.hs
@@ -1,17 +1,13 @@
 module Day4.Advanced where
 import           Data.Function (on)
-import           Day4.Common   (Card (identifier), matchingNumbers, parse)
+import           Day4.Common   (Card (correctNumbers), parse)
 import           Utils.IO      (loadInput)
 
-process :: [Card] -> Int
-process cards = cardsWon cards cards
-
-cardsWon :: [Card] -> [Card] -> Int
-cardsWon allCards (c:cs) = 1 + cardsWon allCards extraCards where
-    thisScore = matchingNumbers c
-    extraCardIds = [(identifier c) .. (identifier c + thisScore - 1)]
-    extraCards = map (allCards !!) extraCardIds ++ cs
-cardsWon _ _ = 0
+cardsWon :: [(Card, Int)] -> Int
+cardsWon ((c, multiplier):cs) = multiplier + cardsWon nextCards where
+    parts = splitAt (correctNumbers c) cs
+    nextCards = map (\(c',m') -> (c', m' + multiplier)) (fst parts) ++ snd parts
+cardsWon _ = 0
 
 main :: IO ()
-main = loadInput >>= print . process . parse
+main = loadInput >>= print . cardsWon . map (,1) . parse
--- Day4/Simple.hs
+++ Day4/Simple.hs
@@ -1,10 +1,10 @@
 module Day4.Simple where
-import           Day4.Common (Card, matchingNumbers, parse)
+import           Day4.Common (Card (correctNumbers), parse)
 import           Utils.IO    (loadInput)
 
 score :: Card -> Int
 score card = if matches == 0 then 0 else 2 ^ (matches - 1) where
-    matches = matchingNumbers card
+    matches = correctNumbers card
 
 main :: IO ()
 main = loadInput >>= print . sum . map score . parse
