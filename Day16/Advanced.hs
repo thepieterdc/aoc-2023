1e0d55d50b30617055948224e8d96a78febf3cbd
Pieter De Clercq <pieter.de.clercq@deliverect.com>
2023-12-16 11:43:12 +0100

    Solve part 2

--- Day16/Advanced.hs
+++ Day16/Advanced.hs
@@ -1,6 +1,21 @@
 module Day16.Advanced where
 
+import qualified Data.Set as Set
+import Day16.Common (Grid, count, parse, walk)
+import Utils.Grid (Coordinate, Direction (..))
 import Utils.IO (loadInput)
 
+run :: Grid -> Int
+run grid = foldr (\s m -> max m $ count $ walk (Set.singleton s) grid) 0 starts
+  where
+    cols = length $ head grid
+    rows = length grid
+
+    fromBottom = [((rows - 1, c), North) | c <- [0 .. cols - 1]]
+    fromLeft = [((r, 0), East) | r <- [0 .. rows - 1]]
+    fromRight = [((r, cols - 1), West) | r <- [0 .. rows - 1]]
+    fromTop = [((0, c), South) | c <- [0 .. cols - 1]]
+    starts = fromBottom ++ fromLeft ++ fromRight ++ fromTop
+
 main :: IO ()
-main = loadInput >>= print
+main = loadInput >>= print . run . parse
--- Day16/Simple.hs
+++ Day16/Simple.hs
@@ -1,85 +1,9 @@
 module Day16.Simple where
 
-import Data.List (intercalate)
-import Data.Maybe (fromJust, fromMaybe, isJust)
-import Data.Semigroup (Min (Min))
-import Data.Set (Set)
 import qualified Data.Set as Set
-import Utils.Filtering (countWhere)
+import Day16.Common (count, parse, walk)
 import Utils.Grid (Direction (..))
 import Utils.IO (loadInput)
-
-data Mirror = Backward | Forward | SplitH | SplitV deriving (Eq, Show)
-
-type Grid = [[([Direction], Maybe Mirror)]]
-
-count :: Grid -> Int
-count = foldr ((+) . countWhere (not . null . fst)) 0
-
-mark :: Direction -> (Int, Int) -> Grid -> Grid
-mark d (r, c) grid = take r grid ++ [take c (grid !! r) ++ [([d], snd $ grid !! r !! c)] ++ drop (c + 1) (grid !! r)] ++ drop (r + 1) grid
-
-parse :: String -> Grid
-parse input = map (map (\c -> ([], parse' c))) $ lines input
-  where
-    parse' :: Char -> Maybe Mirror
-    parse' '\\' = Just Backward
-    parse' '/' = Just Forward
-    parse' '-' = Just SplitH
-    parse' '|' = Just SplitV
-    parse' '.' = Nothing
-
-reflect :: (Int, Int) -> Direction -> Maybe Mirror -> [((Int, Int), Direction)]
--- If the cell is empty, the beam continues in the same direction.
-reflect (r, c) East Nothing = [((r, c + 1), East)]
-reflect (r, c) North Nothing = [((r - 1, c), North)]
-reflect (r, c) South Nothing = [((r + 1, c), South)]
-reflect (r, c) West Nothing = [((r, c - 1), West)]
--- If the cell contains a vertical split mirror, the beam is split vertically if it was going East or West, and continues in the same direction otherwise.
-reflect (r, c) d (Just SplitV) | d == East || d == West = [((r - 1, c), North), ((r + 1, c), South)]
-reflect (r, c) North (Just SplitV) = [((r - 1, c), North)]
-reflect (r, c) South (Just SplitV) = [((r + 1, c), South)]
--- If the cell contains a horizontal split mirror, the beam is split horizontally if it was going North or South, and continues in the same direction otherwise.
-reflect (r, c) d (Just SplitH) | d == North || d == South = [((r, c - 1), West), ((r, c + 1), East)]
-reflect (r, c) East (Just SplitH) = [((r, c + 1), East)]
-reflect (r, c) West (Just SplitH) = [((r, c - 1), West)]
--- If the cell contains a forward mirror, the beam is reflected 90 degrees.
-reflect (r, c) East (Just Forward) = [((r - 1, c), North)]
-reflect (r, c) North (Just Forward) = [((r, c + 1), East)]
-reflect (r, c) South (Just Forward) = [((r, c - 1), West)]
-reflect (r, c) West (Just Forward) = [((r + 1, c), South)]
--- If the cell contains a backward mirror, the beam is reflected 90 degrees.
-reflect (r, c) East (Just Backward) = [((r + 1, c), South)]
-reflect (r, c) North (Just Backward) = [((r, c - 1), West)]
-reflect (r, c) South (Just Backward) = [((r, c + 1), East)]
-reflect (r, c) West (Just Backward) = [((r - 1, c), North)]
-reflect (r, c) d m = error $ show (r, c) ++ " " ++ show d ++ " " ++ show m
-
-render :: Grid -> IO ()
-render g = mapM_ putStrLn $ render' g
-  where
-    render' :: Grid -> [String]
-    render' (r : rs) = map (encode . fst) r : render' rs
-      where
-        encode :: [Direction] -> Char
-        encode ls
-          | length ls > 1 = head $ show ls
-          | null ls = '.'
-          | head ls == East = '>'
-          | head ls == North = '^'
-          | head ls == South = 'v'
-          | head ls == West = '<'
-    render' [] = []
-
-walk :: Set ((Int, Int), Direction) -> Grid -> Grid
-walk q grid
-  | Set.null q = grid
-  | otherwise = if alreadyMarked then walk q' grid else walk (Set.union q' nextPositions) marked
-  where
-    (((r, c), d), q') = Set.deleteFindMin q
-    alreadyMarked = d `elem` fst (grid !! r !! c)
-    nextPositions = Set.fromList [((r', c'), d') | ((r', c'), d') <- reflect (r, c) d (snd $ grid !! r !! c), r' >= 0, r' < length grid, c' >= 0, c' < length (grid !! r'), d' `notElem` fst (grid !! r' !! c')]
-    marked = mark d (r, c) grid
 
 main :: IO ()
 main = loadInput >>= print . count . walk (Set.singleton ((0, 0), East)) . parse
