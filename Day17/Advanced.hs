40047b4c6a43e707444c96a1fb200c34b41b073e
Pieter De Clercq <pieter.de.clercq@deliverect.com>
2023-12-17 12:53:33 +0100

    Solve part 2

--- Day17/Advanced.hs
+++ Day17/Advanced.hs
@@ -1,6 +1,7 @@
-module Day17.Advanced where
+module Day17.Simple where
 
+import Day17.Common (parse, solve)
 import Utils.IO (loadInput)
 
 main :: IO ()
-main = loadInput >>= print
+main = loadInput >>= print . solve 4 10 . parse
--- Utils/Navigation.hs
+++
@@ -1,8 +0,0 @@
--- |
--- Module      : Utils.Navigation
--- Description : Contains methods to handle navigating in a grid.
--- Copyright   : (c) Pieter De Clercq, 2023
--- License     : MIT
---
--- Contains methods to handle navigating in a grid.
-module Utils.Navigation (module Utils.Navigation) where
