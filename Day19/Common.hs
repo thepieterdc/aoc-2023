162de84d1003620f0d609a3c4eabcdb684a0e3b3
Pieter De Clercq <pieter.de.clercq@deliverect.com>
2023-12-22 00:20:56 +0100

    Remove workflow model

--- Day19/Common.hs
+++ Day19/Common.hs
@@ -9,18 +9,16 @@ data Condition = LessThan Int | GreaterThan Int deriving (Eq, Show)
 
 data WorkflowStep = Conditional Char Condition WorkflowStep | Goto String | Accept | Refuse deriving (Eq, Show)
 
-data Workflow = Workflow {name :: String, steps :: [WorkflowStep]} deriving (Eq, Show)
-
 type Rating = Map Char Int
 
-parse :: String -> (Map String Workflow, [Rating])
+parse :: String -> (Map String [WorkflowStep], [Rating])
 parse = doParse parser
   where
     parser = do
       workflows <- some parseWorkflow
       eol
       ratings <- some parseRating
-      return (prune $ prune $ prune $ Map.fromList (map (\wf -> (name wf, wf)) workflows), ratings)
+      return (prune $ prune $ prune $ Map.fromList workflows, ratings)
 
 parseCondition :: Parser Condition
 parseCondition = parseGreaterThan <|> parseLessThan
@@ -45,14 +43,14 @@ parseRating = do
   eol
   return $ Map.fromList [('x', x), ('m', m), ('a', a), ('s', s)]
 
-parseWorkflow :: Parser Workflow
+parseWorkflow :: Parser (String, [WorkflowStep])
 parseWorkflow = do
   name <- some (spot isAsciiLower)
   token '{'
   steps <- parseSteps
   token '}'
   eol
-  return $ Workflow name steps
+  return (name, steps)
   where
     parseSteps = parseMore <|> parseLast
       where
@@ -71,19 +69,19 @@ evaluateCondition :: Condition -> Int -> Bool
 evaluateCondition (LessThan x) y = y < x
 evaluateCondition (GreaterThan x) y = y > x
 
-isAccepted :: Map String Workflow -> Rating -> Bool
+isAccepted :: Map String [WorkflowStep] -> Rating -> Bool
 isAccepted workflows rating = result == Accept
   where
-    result = solve workflows rating $ steps (workflows Map.! "in")
+    result = solve workflows rating $ workflows Map.! "in"
 
-pruneSteps :: Map String Workflow -> [WorkflowStep] -> [WorkflowStep]
+pruneSteps :: Map String [WorkflowStep] -> [WorkflowStep] -> [WorkflowStep]
 pruneSteps wfs wfSteps | isAlwaysAccept wfSteps = [Accept]
   where
     isAlwaysAccept :: [WorkflowStep] -> Bool
     isAlwaysAccept (Accept : _) = True
     isAlwaysAccept (Conditional _ _ Accept : rest) = isAlwaysAccept rest
-    isAlwaysAccept (Conditional _ _ (Goto x) : rest) = isAlwaysAccept $ steps (wfs Map.! x)
-    isAlwaysAccept (Goto target : rest) = isAlwaysAccept $ steps (wfs Map.! target)
+    isAlwaysAccept (Conditional _ _ (Goto x) : rest) = isAlwaysAccept (wfs Map.! x) && isAlwaysAccept rest
+    isAlwaysAccept (Goto target : rest) = isAlwaysAccept (wfs Map.! target)
     isAlwaysAccept [] = True
     isAlwaysAccept _ = False
 pruneSteps wfs wfSteps | isAlwaysRefuse wfSteps = [Refuse]
@@ -91,26 +89,26 @@ pruneSteps wfs wfSteps | isAlwaysRefuse wfSteps = [Refuse]
     isAlwaysRefuse :: [WorkflowStep] -> Bool
     isAlwaysRefuse (Refuse : _) = True
     isAlwaysRefuse (Conditional _ _ Refuse : rest) = isAlwaysRefuse rest
-    isAlwaysRefuse (Conditional _ _ (Goto x) : rest) = isAlwaysRefuse $ steps (wfs Map.! x)
-    isAlwaysRefuse (Goto target : rest) = isAlwaysRefuse $ steps (wfs Map.! target)
+    isAlwaysRefuse (Conditional _ _ (Goto x) : rest) = isAlwaysRefuse (wfs Map.! x) && isAlwaysRefuse rest
+    isAlwaysRefuse (Goto target : rest) = isAlwaysRefuse (wfs Map.! target)
     isAlwaysRefuse [] = True
     isAlwaysRefuse _ = False
 pruneSteps wfs (x : rest) = x : pruneSteps wfs rest
 pruneSteps _ [] = []
 
-prune :: Map String Workflow -> Map String Workflow
+prune :: Map String [WorkflowStep] -> Map String [WorkflowStep]
 prune workflows = Map.map pruneWorkflow workflows
   where
-    pruneWorkflow :: Workflow -> Workflow
-    pruneWorkflow (Workflow name steps) = Workflow name $ pruneSteps workflows steps
+    pruneWorkflow :: [WorkflowStep] -> [WorkflowStep]
+    pruneWorkflow = pruneSteps workflows
 
 score :: Rating -> Int
 score rating = sum $ Map.elems rating
 
-solve :: Map String Workflow -> Rating -> [WorkflowStep] -> WorkflowStep
+solve :: Map String [WorkflowStep] -> Rating -> [WorkflowStep] -> WorkflowStep
 solve _ _ (Accept : _) = Accept
 solve _ _ (Refuse : _) = Refuse
-solve wfs rating ((Goto wf : _)) = solve wfs rating $ steps (wfs Map.! wf)
+solve wfs rating ((Goto wf : _)) = solve wfs rating (wfs Map.! wf)
 solve wfs rating ((Conditional c cond target) : next) = solve wfs rating evaluated
   where
     value = rating Map.! c
--- Day19/Simple.hs
+++ Day19/Simple.hs
@@ -1,10 +1,10 @@
 module Day19.Simple where
 
 import Data.Map (Map)
-import Day19.Common (Rating, Workflow, isAccepted, parse, score)
+import Day19.Common (Rating, WorkflowStep, isAccepted, parse, score)
 import Utils.IO (loadInput)
 
-findAccepted :: (Map String Workflow, [Rating]) -> [Rating]
+findAccepted :: (Map String [WorkflowStep], [Rating]) -> [Rating]
 findAccepted (workflows, ratings) = filter (isAccepted workflows) ratings
 
 main :: IO ()
